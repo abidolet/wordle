1d4a4ad3d8fedb8ba2d10455ea222e7e4bb6fa1f
Ali Uslu <aluslu@z3r3p2.42lyon.fr>
2026-06-27 14:37:41 +0200

    login in progress

--- front/wordle-front/src/Router.tsx
+++ front/wordle-front/src/Router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-
+import Game from "./Game";
+import Home from "./components/Home";
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: "/game",
+    element: <Game />,
   },
- 
+  {
+    path: "/",
+    element: <Home />,
+  },
 ]);
