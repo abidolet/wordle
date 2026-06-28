f2d75117ac2efa47dd58102a886913a0c64ef481
Alexis Bidolet--Foray <abidolet@z3r3p3.42lyon.fr>
2026-06-28 18:19:36 +0200

    add images

--- auth/Models/RevokedTokenModels.cs
+++
@@ -1,8 +0,0 @@
-namespace auth.Models;
-
-public class RevokedToken
-{
-	public int Id { get; set; }
-	public string Jti { get; set; } = default!;
-	public DateTime ExpiresAt { get; set; }
-}
--- auth/Models/TokenModel.cs
+++
@@ -1,7 +0,0 @@
-namespace auth.Models;
-
-public record RegisterRequest(string Email, string Password);
-public record LoginRequest(string Email, string Password);
-public record AuthResponse(string AccessToken, DateTime ExpiresAt);
-public record MessageResponse(string Message);
-public record CheckEmailRequest(string Email);
